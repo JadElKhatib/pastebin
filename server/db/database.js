69d3e01156a68a2baabe8c0295993c8877e27055
JadElKhatib <154486771+JadElKhatib@users.noreply.github.com>
2025-03-09 22:15:51 -0300

    set up dotenv and gitignore in server

--- server/db/database.js
+++ server/db/database.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const mariadb = require("mariadb")
 
 const pool = mariadb.createPool({
-    host: "localhost",
-    user: "root",
-    password: "rootpassword",
-    database: "pastebin_users_master",
-    port: 3306
+    host: process.env.db_host,
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_name,
+    port: process.env.db_port
 })
 
 module.exports = Object.freeze({
